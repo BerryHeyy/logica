4e1bf794fd1efa90cab34222f226b22e972d5be8
BerryHeyy <berkaykaplanbk@hotmail.com>
2021-02-22 11:16:39 +0100

    Initial commit

---
+++ src/main/java/io/github/berryheyy/logica/LogicaClient.java
@@ -0,0 +1,11 @@
+package io.github.berryheyy.logica;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LogicaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
